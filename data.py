b72f707028b429c8fb9d1180220866df59ed8106
AMCorvi <am@corvi.com>
2018-03-17 07:14:43 -0400

    - Added: Intial Templating - Added: Intial Routes - Added: Place Holder Data for 'Articles' Route

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [{
+    "id": 1,
+    "body":
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
+    "title": "Nullam sit amet turpis elementum ligula vehicula consequat.",
+    "author": "Udall Covil",
+    "created_at": "2018-05-23"
+  },
+  {
+    "id": 2,
+    "body":
+      "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    "title": "Phasellus id sapien in sapien iaculis congue.",
+    "author": "Blake Glascott",
+    "created_at": "2017-11-04"
+  },
+  {
+    "id": 3,
+    "body":
+      "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+    "title": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    "author": "Petrina Arkwright",
+    "created_at": "2017-09-07"
+  }
+]
+    return articles
